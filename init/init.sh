aaf7192d583e613d31fa9899d022080b3f039f92
Matthew DiMaiolo <red.beard.aero@gmail.com>
2021-06-11 14:38:10 -0500

    Changed SU2_HOME to SU2

--- init/init.sh
+++ init/init.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-export SU2_HOME=/data/SU2_HOME
+export SU2_HOME=/data/SU2
 export SU2_RUN=/usr/local/SU2/bin
 export PATH=$SU2_RUN:$PATH
 export PYTHONPATH=$SU2_RUN:$PYTHONPATH
 
-cd /data/SU2_HOME
+cd /data/SU2
 
-sudo chmod -R 0777 /data/SU2_HOME
+sudo chmod -R 0777 /data/SU2
